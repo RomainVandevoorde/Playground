7a66347dc8aed18b50c23097fab3f642d37ee643
unknown <symael@live.fr>
2018-05-24 23:12:08 +0200

    Mastermind: stable algorithm

--- Mastermind/mastermind.js
+++ Mastermind/mastermind.js
@@ -1,17 +1,40 @@
 window.onload = () => {
-  let pegs = document.getElementsByClassName('peg');
-
-  // for(let i =0; i < pegs.length; i++) {
-  //   pegs[i].innerHTML = 'x';
-  // }
 
   let game = new Mastermind();
 
   let controlButton = document.getElementsByTagName('button')[0];
   controlButton.addEventListener('click', function(){
-    game.newGuess();
-  })
-}
+    try {
+      // Invalid input, do nothing
+      // if(!validInput(game.colors.length-1)) return;
+
+      let res = game.newGuess();
+      console.log(res);
+      // Game has ended
+      if (res[0]) {
+        if (res[1]) document.getElementById('messageBox').getElementsByTagName('p')[0].innerHTML = 'You won !';
+        else document.getElementById('messageBox').getElementsByTagName('p')[0].innerHTML = 'You lost !';
+      }
+    } catch (e) {
+      document.getElementById('messageBox').getElementsByTagName('p')[0].innerHTML = e;
+    }
+  });
+};
+
+const validInput = (upperLimit) => {
+  let pegs = document.getElementById('userGuess').getElementsByTagName('input');
+  for(let i = 0; i < pegs.length; i++) {
+
+    // Check if input is a valid integer
+    let value = parseInt(pegs[i].value);
+    if(!Number.isInteger(value)) return false;
+
+    // Check if integer is within limits
+    if(value >= 0 && value <= upperLimit) this.currentGuess.push(value);
+    else return false;
+
+  }
+};
 
 class Mastermind {
 
@@ -35,27 +58,6 @@ class Mastermind {
     console.log('PegSet generated: '+this.pegSet);
   }
 
-  // Returns the number of times a key appears in an array
-  arrayKeysAmount(array, key) {
-    let amount = 0;
-    for(let i = 0; i < array.length; i++) {
-      if(array[i] === key) amount++;
-    }
-    return amount;
-  }
-
-  // Called when user validates a new guess
-  set newGuess(guess) {
-    if(!isValidGuess(guess)) {
-      console.log('nope');
-      return;
-    }
-    console.log('newGuess');
-    this.currentGuess = guess;
-    this.putCurrentGuessInUI();
-    this.guessNb++;
-  }
-
   getNewGuess() {
     this.currentGuess = [];
     let pegs = document.getElementById('userGuess').getElementsByTagName('input');
@@ -64,61 +66,67 @@ class Mastermind {
       if(value >= 0 && value <= 5) this.currentGuess.push(value);
       else {
         console.log('Invalid Input');
-        return;
+        throw "Exception: Invalid Input";
+        // return;
       }
     }
     console.log('Valid user input, currentGuess set to: '+this.currentGuess);
   }
 
   // Main game loop (triggered when user enters a new guess)
+  // Returns an array: [game ended, game won]
   newGuess() {
+    document.getElementById('messageBox').getElementsByTagName('p')[0].innerHTML = "";
     this.getNewGuess();
     this.putCurrentGuessInUI();
+    let botTips = this.getBotTips();
+    this.putTipsInUI(botTips);
+
+    if(botTips[0] === 4) return [true, true];
+    if(this.guessNb >= 10) return [true, false];
+
     this.guessNb++;
-    this.botTips();
+
+    return [false, false];
   }
 
-  // Generates bot's response (number of black and white pegs)
-  botTips() {
+  getBotTips () {
     let nbCorrect = 0;
     let nbCorrectColor = 0;
 
-    let pegSet = this.pegSet;
-    let correctColors = [];
-    let correct = [];
+    let treatedSet = {0:false,1:false,2:false,3:false};
+    let treatedGuess = {0:false,1:false,2:false,3:false};
 
+    // Find perfect matches
     for (let i = 0; i < 4; i++) {
-      let guessVal = this.currentGuess[i];
-      console.log('--- guessVal: '+guessVal);
-
-      let nbInCorrectColors = this.arrayKeysAmount(correctColors, guessVal);
-      let nbInCorrect = this.arrayKeysAmount(correct, guessVal);
-      let nbInPegSet = this.arrayKeysAmount(this.pegSet, guessVal);
-
+      // console.log('-1- guessVal: '+this.currentGuess[i]);
       if(this.currentGuess[i] === this.pegSet[i]) {
         nbCorrect++;
-        correct.push(guessVal);
-        console.log('=> Correct');
+        treatedSet[i] = true;
+        treatedGuess[i] = true;
+        // console.log('=> Correct');
       }
-      else if(pegSet.indexOf(this.currentGuess[i]) !== -1) {
+    }
 
-        console.log('CorrectColor ?');
-        console.log('nbInCorrectColors: '+nbInCorrectColors);
-        console.log('nbInPegSet: '+nbInPegSet);
-        if(nbInCorrectColors+nbInCorrect < nbInPegSet) {
-          console.log('=> CorrectColor');
-          correctColors.push(guessVal);
+    // Find partial matches
+
+    for (let i = 0; i < 4; i++) {
+      if(treatedGuess[i]) continue; // If treated, this guess was a perfect match => go to next loop
+
+      for (let j = 0; j < 4; j++) {
+        if(treatedSet[j]) continue; // If this element in the pegSet has been treated, don't treat it again
+        if(this.currentGuess[i] === this.pegSet[j]) {
           nbCorrectColor++;
+          treatedSet[j] = true;
+          treatedGuess[i] = true;
+          break; // We found a match, go to next userGuess item
         }
-      }
-
-      while(nbCorrect+nbCorrectColor > nbInPegSet) {
-        console.log('-1 correctColor');
-        nbCorrectColor--;
       }
     }
 
     console.log('Correct: '+nbCorrect+' / correctColor: '+nbCorrectColor);
+    return [nbCorrect, nbCorrectColor];
+
   }
 
   // Puts the current guess on the gameboard
@@ -127,6 +135,14 @@ class Mastermind {
     for(let i = 0; i < pegs.length; i++) {
       pegs[i].innerHTML = this.currentGuess[i];
     }
+  }
+
+  putTipsInUI(tips) {
+    let row = this.guessesContainer.getElementsByClassName('row')[this.guessNb-1];
+    let correctBlock = row.getElementsByClassName('correct')[0];
+    let correctColorBlock = row.getElementsByClassName('correctColor')[0];
+    correctBlock.innerHTML = tips[0];
+    correctColorBlock.innerHTML = tips[1];
   }
 
 }
